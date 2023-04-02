384bfcdad5d9b2cc40d4ad2936e4dde2150a7196
Sids-03 <110683058+Sids-03@users.noreply.github.com>
2023-04-02 15:51:29 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define PI atan(1)*4
+#define set_bits __builtin_popcountllO
+#define all(x) (x).begin(), (x).end()
+#define vi vector<int>
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define rvsort(a) sort(all(a),greater<int>())
+#define read(a,n) for(int i = 0 ; i < n ; i ++){ cin >> a[i];}
+#define printv(a) for(auto it: a){cout << it << " ";} cout << endl;
+#define ms(arr, v) memset(arr, v, sizeof(arr))
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll uid(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
+
+void solve(){
+   
+}
+
+int main() {
+
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+
+
+ll t = 1;
+cin >> t;
+for(int i = 1 ; i <= t ; i++){
+//google(i);
+  solve();
+}
+return 0;
+}
+
+
